e5adcbf79e9ca590f29345cd6dec30aa7ca84070
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-05-09 14:33:56 +0200

    Update project.clj with the repository URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject siili/memocks "0.1.0-SNAPSHOT"
   :description "Simple mocking library to use in your clojure tests."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/siilisolutions-pl/memocks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
