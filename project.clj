25a9a4a1b31ce9a33847cc9cf30e866575cb55a3
Konrad Mrożek <kmrozek+git@proton.me>
2024-09-08 15:42:38 +0200

    Migrate to Clojure deps project
    I've rewritten .gitignore and remove project.cl

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject com.siili/memocks "0.1.0"
-  :description "Simple mocking library to use in your clojure tests."
-  :url "https://github.com/siilisolutions-pl/memocks"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
